d99426eb67ef001c31089b90be48561908b2b744
lazzerex <nambinh236@gmail.com>
2026-06-20 17:06:41 +0700

    feat: add main server implementation with database and Redis connections

---
+++ cmd/server/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"github.com/redis/go-redis/v9"
+
+	"github.com/lazzerex/gitrpg/internal/config"
+	"github.com/lazzerex/gitrpg/internal/server"
+)
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}))
+
+	_ = godotenv.Load()
+
+	cfg, err := config.Load()
+	if err != nil {
+		logger.Error("config load failed", "error", err)
+		os.Exit(1)
+	}
+
+	if cfg.Server.Env == "development" {
+		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	}
+
+	db, err := pgxpool.New(context.Background(), cfg.Database.URL)
+	if err != nil {
+		logger.Error("db connect failed", "error", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	if err := db.Ping(context.Background()); err != nil {
+		logger.Error("db ping failed", "error", err)
+		os.Exit(1)
+	}
+	logger.Info("db connected")
+
+	opts, err := redis.ParseURL(cfg.Redis.URL)
+	if err != nil {
+		logger.Error("redis url parse failed", "error", err)
+		os.Exit(1)
+	}
+	rdb := redis.NewClient(opts)
+	defer rdb.Close()
+
+	if err := rdb.Ping(context.Background()).Err(); err != nil {
+		logger.Error("redis ping failed", "error", err)
+		os.Exit(1)
+	}
+	logger.Info("redis connected")
+
+	srv := server.New(cfg, db, rdb, logger)
+
+	if err := srv.LoadTemplates("web/templates/*.html"); err != nil {
+		logger.Error("template load failed", "error", err)
+		os.Exit(1)
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.Start(); err != nil {
+			logger.Error("server error", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	<-quit
+	logger.Info("shutting down")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Error("shutdown error", "error", err)
+	}
+}
