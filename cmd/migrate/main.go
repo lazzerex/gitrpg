30a605482d77ece67fdf60ce6a83cb08b681e391
lazzerex <nambinh236@gmail.com>
2026-06-20 17:06:32 +0700

    feat: implement migration command with database connection and logging

---
+++ cmd/migrate/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"log/slog"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/pressly/goose/v3"
+
+	"github.com/lazzerex/gitrpg/internal/config"
+)
+
+func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	_ = godotenv.Load()
+
+	cfg, err := config.Load()
+	if err != nil {
+		logger.Error("config load failed", "error", err)
+		os.Exit(1)
+	}
+
+	db, err := sql.Open("pgx", cfg.Database.URL)
+	if err != nil {
+		logger.Error("db open failed", "error", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	if err := goose.SetDialect("postgres"); err != nil {
+		logger.Error("set dialect failed", "error", err)
+		os.Exit(1)
+	}
+
+	cmd := "up"
+	if len(os.Args) > 1 {
+		cmd = os.Args[1]
+	}
+
+	migrationsDir := "migrations"
+	if err := goose.RunContext(context.Background(), cmd, db, migrationsDir); err != nil {
+		logger.Error("migration failed", "cmd", cmd, "error", err)
+		os.Exit(1)
+	}
+
+	logger.Info("migration done", "cmd", cmd)
+}
